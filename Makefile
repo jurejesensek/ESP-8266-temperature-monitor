8b424276d80031cfb13e4969e7ea3a79b1e5563e
BSO <ml9987@student.uni-lj.si>
2018-05-24 21:07:31 +0200

    mqtt implemented

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 PROGRAM=temperature_monitor
-EXTRA_COMPONENTS=extras/i2c extras/rboot-ota extras/RF24 extras/bmp280
+EXTRA_COMPONENTS=extras/i2c extras/rboot-ota extras/RF24 extras/bmp280 extras/paho_mqtt_c
 include ../esp-open-rtos/common.mk
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "espressif/esp_common.h"
 #include "esp/uart.h"
 
+#include <string.h>
+
 #include "FreeRTOS.h"
 #include "task.h"
 
@@ -11,11 +13,17 @@
 
 #include "bmp280_sensor.h"
 #include "constants.h"
+#include "secret_constants.h"
 
 #include <espressif/esp_sta.h>
 #include <espressif/esp_wifi.h>
 #include <semphr.h>
 #include "wifi_config/ssid_config.h"
+
+extern "C" {
+#include "paho_mqtt_c/MQTTESP8266.h"
+#include "paho_mqtt_c/MQTTClient.h"
+}
 
 #include "nrf_transmitter.h"
 
@@ -24,6 +32,8 @@ TempMonitor::Bmp280_temp_sensor temp_sensor;
 TempMonitor::NRF_transmitter nrf_transmitter;
 
 SemaphoreHandle_t wifi_alive;
+QueueHandle_t publish_queue;
+#define PUB_MSG_LEN 16
 
 void readTemp(void *pvParameters)
 {
@@ -31,9 +41,142 @@ void readTemp(void *pvParameters)
 	{
         float temp = temp_sensor.read();
         printf("temp: %f\n", temp);
-		nrf_transmitter.transmit_nrf24();
         vTaskDelay(1000 / portTICK_PERIOD_MS); // sleep
 	}
+}
+
+static void  beat_task(void *pvParameters)
+{
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    char msg[PUB_MSG_LEN];
+    int count = 0;
+
+    while (1) {
+        vTaskDelayUntil(&xLastWakeTime, 10000 / portTICK_PERIOD_MS);
+        printf("beat\n");
+        snprintf(msg, PUB_MSG_LEN, "Beat %d\n", count++);
+        if (xQueueSend(publish_queue, (void *)msg, 0) == pdFALSE) {
+            printf("Publish queue overflow.\n");
+        }
+    }
+}
+
+static void  topic_received(mqtt_message_data_t *md)
+{
+    int i;
+    mqtt_message_t *message = md->message;
+    printf("Received: ");
+    for( i = 0; i < md->topic->lenstring.len; ++i)
+        printf("%c", md->topic->lenstring.data[ i ]);
+
+    printf(" = ");
+    for( i = 0; i < (int)message->payloadlen; ++i)
+        printf("%c", ((char *)(message->payload))[i]);
+
+    printf("\n");
+}
+
+static const char *  get_my_id(void)
+{
+    // Use MAC address for Station as unique ID
+    static char my_id[13];
+    static bool my_id_done = false;
+    int8_t i;
+    uint8_t x;
+    if (my_id_done)
+        return my_id;
+    if (!sdk_wifi_get_macaddr(STATION_IF, (uint8_t *)my_id))
+        return NULL;
+    for (i = 5; i >= 0; --i)
+    {
+        x = my_id[i] & 0x0F;
+        if (x > 9) x += 7;
+        my_id[i * 2 + 1] = x + '0';
+        x = my_id[i] >> 4;
+        if (x > 9) x += 7;
+        my_id[i * 2] = x + '0';
+    }
+    my_id[12] = '\0';
+    my_id_done = true;
+    return my_id;
+}
+
+static void  mqtt_task(void *pvParameters)
+{
+    int ret         = 0;
+    struct mqtt_network network;
+    mqtt_client_t client   = mqtt_client_default;
+    char mqtt_client_id[20];
+    uint8_t mqtt_buf[100];
+    uint8_t mqtt_readbuf[100];
+    mqtt_packet_connect_data_t data = mqtt_packet_connect_data_initializer;
+
+    mqtt_network_new( &network );
+    memset(mqtt_client_id, 0, sizeof(mqtt_client_id));
+    strcpy(mqtt_client_id, "ESP-");
+    strcat(mqtt_client_id, get_my_id());
+
+    while(1) {
+        xSemaphoreTake(wifi_alive, portMAX_DELAY);
+        printf("%s: started\n", __func__);
+        printf("%s: (Re)connecting to MQTT server %s ... ",__func__,
+               MQTT_HOST);
+        ret = mqtt_network_connect(&network, MQTT_HOST, MQTT_PORT);
+        if( ret ){
+            printf("error: %d\n", ret);
+            taskYIELD();
+            continue;
+        }
+        printf("done\n");
+        mqtt_client_new(&client, &network, 5000, mqtt_buf, 100,
+                      mqtt_readbuf, 100);
+
+        data.willFlag       = 0;
+        data.MQTTVersion    = 3;
+        data.clientID.cstring   = mqtt_client_id;
+        data.username.cstring   = MQTT_USER;
+        data.password.cstring   = MQTT_PASS;
+        data.keepAliveInterval  = 10;
+        data.cleansession   = 0;
+        printf("Send MQTT connect ... ");
+        ret = mqtt_connect(&client, &data);
+        if(ret){
+            printf("error: %d\n", ret);
+            mqtt_network_disconnect(&network);
+            taskYIELD();
+            continue;
+        }
+        printf("done\n");
+        mqtt_subscribe(&client, "dev/test", MQTT_QOS1, topic_received);
+        xQueueReset(publish_queue);
+
+        while(1){
+
+            char msg[PUB_MSG_LEN - 1] = "\0";
+            while(xQueueReceive(publish_queue, (void *)msg, 0) ==
+                  pdTRUE){
+                printf("got message to publish\n");
+                mqtt_message_t message;
+                message.payload = msg;
+                message.payloadlen = PUB_MSG_LEN;
+                message.dup = 0;
+                message.qos = MQTT_QOS1;
+                message.retained = 0;
+                ret = mqtt_publish(&client, "dev/test", &message);
+                if (ret != MQTT_SUCCESS ){
+                    printf("error while publishing message: %d\n", ret );
+                    break;
+                }
+            }
+
+            ret = mqtt_yield(&client, 1000);
+            if (ret == MQTT_DISCONNECTED)
+                break;
+        }
+        printf("Connection dropped, request restart\n");
+        mqtt_network_disconnect(&network);
+        taskYIELD();
+    }
 }
 
 void wifi_task(void *pvParameters)
@@ -117,12 +260,15 @@ void user_init(void)
 	
 	//setup_nrf();
 	vSemaphoreCreateBinary(wifi_alive);
-	//publish_queue = xQueueCreate(3, PUB_MSG_LEN);
+	publish_queue = xQueueCreate(3, PUB_MSG_LEN);
 
     // Create user interface task
 	
 	xTaskCreate(&wifi_task, "wifi_task", 265, nullptr, 2, nullptr);
 	//xTaskCreate(&mqtt_task, "mqtt_task", 1024, NULL, 4, NULL);
     xTaskCreate(readTemp, "readTemp", 265, nullptr, 2, nullptr);
+	
+    xTaskCreate(&beat_task, "beat_task", 256, NULL, 3, NULL);
+    xTaskCreate(&mqtt_task, "mqtt_task", 1024, NULL, 4, NULL);
 
 }
